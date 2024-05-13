aa80cf22e2a75b2ff1c04695a375f15b64e71f8d
wipdev-tech <wipdev.tech@gmail.com>
2024-05-13 18:21:11 +0300

    Generate accounts/auth boilerplate

--- lib/talentlink_web/router.ex
+++ lib/talentlink_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TalentlinkWeb.Router do
   use TalentlinkWeb, :router
 
+  import TalentlinkWeb.AccountAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule TalentlinkWeb.Router do
     plug :put_root_layout, html: {TalentlinkWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_account
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule TalentlinkWeb.Router do
 
       live_dashboard "/dashboard", metrics: TalentlinkWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", TalentlinkWeb do
+    pipe_through [:browser, :redirect_if_account_is_authenticated]
+
+    live_session :redirect_if_account_is_authenticated,
+      on_mount: [{TalentlinkWeb.AccountAuth, :redirect_if_account_is_authenticated}] do
+      live "/accounts/register", AccountRegistrationLive, :new
+      live "/accounts/log_in", AccountLoginLive, :new
+      live "/accounts/reset_password", AccountForgotPasswordLive, :new
+      live "/accounts/reset_password/:token", AccountResetPasswordLive, :edit
+    end
+
+    post "/accounts/log_in", AccountSessionController, :create
+  end
+
+  scope "/", TalentlinkWeb do
+    pipe_through [:browser, :require_authenticated_account]
+
+    live_session :require_authenticated_account,
+      on_mount: [{TalentlinkWeb.AccountAuth, :ensure_authenticated}] do
+      live "/accounts/settings", AccountSettingsLive, :edit
+      live "/accounts/settings/confirm_email/:token", AccountSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", TalentlinkWeb do
+    pipe_through [:browser]
+
+    delete "/accounts/log_out", AccountSessionController, :delete
+
+    live_session :current_account,
+      on_mount: [{TalentlinkWeb.AccountAuth, :mount_current_account}] do
+      live "/accounts/confirm/:token", AccountConfirmationLive, :edit
+      live "/accounts/confirm", AccountConfirmationInstructionsLive, :new
     end
   end
 end
---
+++ priv/repo/migrations/20240513105327_create_accounts_auth_tables.exs
@@ -0,0 +1,27 @@
+defmodule Talentlink.Repo.Migrations.CreateAccountsAuthTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :email, :string, null: false, collate: :nocase
+      add :hashed_password, :string, null: false
+      add :confirmed_at, :naive_datetime
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:accounts, [:email])
+
+    create table(:accounts_tokens, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :account_id, references(:accounts, type: :binary_id, on_delete: :delete_all), null: false
+      add :token, :binary, null: false, size: 32
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(updated_at: false)
+    end
+
+    create index(:accounts_tokens, [:account_id])
+    create unique_index(:accounts_tokens, [:context, :token])
+  end
+end
