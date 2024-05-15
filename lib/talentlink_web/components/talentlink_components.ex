b67693aa789b733486ce7ae4f9ea4da54d318bda
wipdev-tech <wipdev.tech@gmail.com>
2024-05-15 19:24:52 +0300

    Make job accordion dynamic based on mock data

--- lib/talentlink_web/components/talentlink_components.ex
+++ lib/talentlink_web/components/talentlink_components.ex
@@ -53,4 +53,64 @@ defmodule TalentlinkWeb.TalentlinkComponents do
     </button>
     """
   end
+
+  def job_accordion(assigns) do
+    ~H"""
+    <div class="hs-accordion-group my-5">
+      <%= for {job_id, job_title, job_desc} <- @jobs do %>
+      <div
+        class="hs-accordion bg-white border -mt-px first:rounded-t-lg last:rounded-b-lg dark:bg-neutral-800 dark:border-neutral-700"
+        id={"hs-bordered-heading-#{job_id}"}
+      >
+        <button
+          class="hs-accordion-toggle hs-accordion-active:text-blue-600 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 py-4 px-5 hover:text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:outline-none dark:focus:text-neutral-400"
+          aria-controls={"hs-basic-bordered-collapse-#{job_id}"}
+        >
+          <svg
+            class="hs-accordion-active:hidden block size-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5v14"></path>
+          </svg>
+          <svg
+            class="hs-accordion-active:block hidden size-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M5 12h14"></path>
+          </svg>
+        <%= job_title %>
+        </button>
+        <div
+          id={"hs-basic-bordered-collapse-#{job_id}"}
+          class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+          aria-labelledby={"hs-bordered-heading-#{job_id}"}
+        >
+          <div class="pb-4 px-5">
+            <p class="text-gray-800 dark:text-neutral-200">
+              <%= job_desc %>
+            </p>
+          </div>
+        </div>
+      </div>
+      <% end %>
+    </div>
+    """
+  end
 end
