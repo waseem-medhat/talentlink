a30b41ea9495007462d3014ce676f09be035733e
wipdev-tech <wipdev.tech@gmail.com>
2024-05-15 21:08:03 +0300

    Adjust UI + add dialyxir

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Talentlink.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
