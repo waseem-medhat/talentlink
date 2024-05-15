2b2f622541ffd32bf5a7326f7d3074d9077c87cc
wipdev-tech <wipdev.tech@gmail.com>
2024-05-15 17:48:48 +0300

    Adjust home UI + make theme switcher component

--- lib/talentlink_web.ex
+++ lib/talentlink_web.ex
@@ -85,6 +85,7 @@ defmodule TalentlinkWeb do
       import Phoenix.HTML
       # Core UI components and translation
       import TalentlinkWeb.CoreComponents
+      import TalentlinkWeb.TalentlinkComponents
       import TalentlinkWeb.Gettext
 
       # Shortcut for generating JS commands
